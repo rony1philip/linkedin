9820570641ebbe87804a201405c48b521ea74176
rony1philip <26e4u69@gmail.com>
2024-04-01 10:48:09 +0300

    TODO- way labels are printed 2 times? TODO - printing the correct label (row 64 e.g finding correct label) TODO - if statements (with/without questions) TODO - < fieldset >

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-
 import time
 
 chrome_options = webdriver.ChromeOptions()
